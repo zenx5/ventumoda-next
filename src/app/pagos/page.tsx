f4a8bcf21fedb9c3a561773427c48e3600b1d646
Octavio Martinez <omartinez1618@gmail.com>
2025-02-28 23:51:52 -0400

    feat: Implementar página de pagos
    Se agregó una página de pagos con diferentes métodos de pago (Banesco, BVD, Binance, Zelle).  Se incluyeron imágenes para cada método y se creó un componente reutilizable 'PaymentCard'.
    
    [by Kommiter]

--- src/app/pagos/page.tsx
+++ src/app/pagos/page.tsx
@@ -1,4 +1,69 @@
+import Image from "next/image";
+import SectionBase from "@/components/SectionBase";
+import PaymentCard from "@/components/PaymentCard";
+
+import paymenst from "@/assets/payments.jpg"
+import Banesco from "@/assets/Banesco.png"
+import Bdv from "@/assets/bancovenezuelalogo.png"
+import Binance from "@/assets/binance.png"
+import Zelle from "@/assets/zelle.png"
+
 export default function Page() {
 
-    return <main>Pagos</main>
+    return <SectionBase>
+        <div className="grid grid-cols-2 mt-5">
+            <Image src={paymenst} alt="" className="col-span-1"/>
+            <div className="col-span-1 flex flex-col justify-center gap-10 mx-6">
+                <h1 className="text-3xl font-bold">¡Hacer pagos nunca ha sido tan sencillo!</h1>
+                <p className="text-lg">Te ofrecemos múltiples opciones para que elijas la que mejor se adapte a tu comodidad</p>
+            </div>
+        </div>
+        <div className="grid grid-cols-4 gap-5 py-8">
+            <span className="col-span-1">
+                <PaymentCard src={Banesco} className="flex flex-col gap-1 pt-2">
+                    <span className="font-bold">Numero de cuenta</span>
+                    <span className="">0000000000000000000000</span>
+                    <span className="font-bold">Cuenta corriente</span>
+                    <span className="flex flex-row gap-2">
+                        <span className="font-bold">Pago Movil</span>
+                        <span className="">Pago Movil</span>
+                    </span>
+                </PaymentCard>
+            </span>
+            <span className="col-span-1">
+                <PaymentCard src={Bdv} className="flex flex-col gap-1 pt-2">
+                    <span className="font-bold">Numero de cuenta</span>
+                    <span className="">0000000000000000000000</span>
+                    <span className="font-bold">Cuenta corriente</span>
+                    <span className="flex flex-row gap-2">
+                        <span className="font-bold">Pago Movil</span>
+                        <span className="">Pago Movil</span>
+                    </span>
+                </PaymentCard>
+            </span>
+            <span className="col-span-1">
+                <PaymentCard src={Binance} className="flex flex-col gap-1 pt-2">
+                    <span className="font-bold">Numero de cuenta</span>
+                    <span className="">0000000000000000000000</span>
+                    <span className="font-bold">Cuenta corriente</span>
+                    <span className="flex flex-row gap-2">
+                        <span className="font-bold">Pago Movil</span>
+                        <span className="">Pago Movil</span>
+                    </span>
+                </PaymentCard>
+            </span>
+            <span className="col-span-1">
+                <PaymentCard src={Zelle} className="flex flex-col gap-1 pt-2">
+                    <span className="font-bold">Numero de cuenta</span>
+                    <span className="">0000000000000000000000</span>
+                    <span className="font-bold">Cuenta corriente</span>
+                    <span className="flex flex-row gap-2">
+                        <span className="font-bold">Pago Movil</span>
+                        <span className="">Pago Movil</span>
+                    </span>
+                </PaymentCard>
+            </span>
+        </div>
+    </SectionBase>
 }
+
---
+++ src/components/PaymentCard.tsx
@@ -0,0 +1,12 @@
+import Image, { StaticImageData } from "next/image"
+import { ReactNode } from "react"
+
+export default function PaymentCard({ src,className, children }:{ src:string|StaticImageData, className?:string, children:ReactNode }) {
+
+    return <div className="shadow p-4 divide-y-2 flex flex-col gap-2">
+        <Image src={src} alt="" className="h-10 w-fit mx-auto my-4"/>
+        <div className={className}>
+            { children }
+        </div>
+    </div>
+}
