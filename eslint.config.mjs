a24e1e94294e32aa59eb57686ed84efa4ece550e
Octavio Martinez <omartinez1618@gmail.com>
2025-11-24 02:15:20 +0000

    eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rule: {
+    rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
     }
