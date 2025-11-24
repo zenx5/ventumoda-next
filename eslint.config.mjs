793970d2b725455fb025e99b417d9522f8f908b1
Octavio Martinez <omartinez1618@gmail.com>
2025-11-24 02:27:01 +0000

    eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,17 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
+  
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn", 
+      "@typescript-eslint/no-explicit-any": "warn"
     }
-  }
+
+  })
 ];
 
 export default eslintConfig;
